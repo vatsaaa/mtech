abccf44c30655ac7b8732bc9784c854d41f1ce84
Ankur Vatsa <ankur.vatsa@gmail.com>
2024-02-05 18:18:42 +0530

    Same file will run any of the algos

--- semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/main.py
+++ semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/main.py
@@ -1,3 +1,4 @@
+import argparse
 from pprint import pprint
 
 ## Import project libraries
@@ -8,13 +9,22 @@ from GBFS.gbfs import greedy_best_first_search
 from GENETIC_ALGORITHM.geneticalgo import genetic_algorithm
 
 if __name__ == "__main__":
-    print("Running Greedy Best First Search:")
-    env = GridEnvironment(grid)
-    pprint(env.grid) # Print the grid
-    
-    path, total_cost = greedy_best_first_search(env)
-    print("Path taken by the agent:",path)
-    print("Total path cost:", total_cost)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--genetic", action="store_true", help="Run Genetic Algorithm")
+    parser.add_argument("-b", "--gbfs", action="store_true", help="Run Greedy Best First Search")
+    args = parser.parse_args()
 
-    print("\n\n\nRunning Genetic Algorithm:")
-    genetic_algorithm(env)
+    env = GridEnvironment(grid)
+    pprint(env.grid) # Print the grid        
+
+    if args.gbfs:
+        print("Running Greedy Best First Search:")
+        path, total_cost = greedy_best_first_search(env)
+        print("Path taken by the agent:",path)
+        print("Total path cost:", total_cost)
+    elif args.genetic:
+        print("\n\n\nRunning Genetic Algorithm:")
+        env = GridEnvironment(grid)
+        genetic_algorithm(env)
+    else:
+        print("Please select an algorithm to run. Use -h for help.")
