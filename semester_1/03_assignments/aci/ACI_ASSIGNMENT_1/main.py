c7a87934826f6298cfc29f65e9651ac5badfe0ed
Ankur Vatsa <ankur.vatsa@gmail.com>
2024-02-10 01:46:28 +0530

    Run both algos together

--- semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/main.py
+++ semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/main.py
@@ -1,6 +1,10 @@
 import argparse
 from pprint import pprint
 
+from utils.grid import grid
+from utils.GridEnvironment import GridEnvironment
+from Algorithms.GBFSearchAlgorithm import GBFSearchAlgorithm
+from Algorithms.GeneticSearchAlgorithm import GeneticSearchAlgorithm
 from Algorithms.SearchAlgorithmFactory import SearchAlgorithmFactory
 
 def main():
@@ -26,10 +30,26 @@ def main():
     parser.add_argument("-d", "--display", action="store_true", help="Display grid")
     args = parser.parse_args()
 
-    search_algorithm = SearchAlgorithmFactory.create_search_algorithm(args)
-    path, total_cost = search_algorithm.search()
-    print("Path taken by the agent:", path)
-    print("Total path cost:", total_cost)
+    env = GridEnvironment(grid, args.display)
+
+    if(args.genetic == False) and (args.gbfs == False):
+        print("Running both Genetic Algorithm and Greedy Best First Search")
+        gbfs = GBFSearchAlgorithm(env)
+        gbfs_path, gbfs_cost = gbfs.search()
+
+        print("Path taken by the agent using Greedy Best First Search:", gbfs_path)
+        print("Total path cost using Greedy Best First Search:", gbfs_cost)
+
+        genetic_search = GeneticSearchAlgorithm(env)
+        gs_path, gs_cost = genetic_search.search()
+
+        print("Path taken by the agent using Genetic Algorithm:", gs_path)
+        print("Total path cost using Genetic Algorithm:", gs_cost)
+    else:
+        search_algorithm = SearchAlgorithmFactory.create_search_algorithm(args)
+        path, total_cost = search_algorithm.search()
+        print("Path taken by the agent:", path)
+        print("Total path cost:", total_cost)
 
 if __name__ == "__main__":
     main()
