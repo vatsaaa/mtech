c4452e788b9bc1cbcfed46ad79f54fd45b7b142d
Ankur Vatsa <ankur.vatsa@gmail.com>
2024-02-10 01:35:07 +0530

    Bring back code for track time and space

--- semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/utils/grid.py
+++ semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/utils/grid.py
@@ -1,5 +1,4 @@
-import time
-import psutil
+import time, resource
 
 grid = [
     ['S', '.', '.', '.', '#', '#', '.', '.'],
@@ -15,16 +14,16 @@ grid = [
 def track_time_and_space(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        start_memory = psutil.Process().memory_info().rss / 1024 / 1024  # in MB
+        start_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
         result = func(*args, **kwargs)
         end_time = time.time()
-        end_memory = psutil.Process().memory_info().rss / 1024 / 1024  # in MB
+        end_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
         execution_time = end_time - start_time
-        memory_usage = end_memory - start_memory
+        memory_usage = (end_memory - start_memory) / 1024  # Convert to kilobytes
 
         print(f"Execution time: {execution_time} seconds")
-        print(f"Memory usage: {memory_usage} MB")
+        print(f"Memory usage: {memory_usage} KB")
 
         return result
 
