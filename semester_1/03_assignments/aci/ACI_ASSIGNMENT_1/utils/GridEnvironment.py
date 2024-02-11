5b0354d29625cca3541bdce432ebf00a30501f4c
prasenjitSamantaBits <2023aa05256@wilp.bits-pilani.ac.in>
2024-02-11 20:13:12 +0530

    Updating code to display path in multicolor

--- semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/utils/GridEnvironment.py
+++ semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/utils/GridEnvironment.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import random
+import sys
 
 class GridEnvironment:
     def __init__(self, grid: list[list[str]], display: bool = False):
@@ -18,28 +20,38 @@ class GridEnvironment:
         find_start_and_goal()
 
     def visualize(self, path):
-        # Define colors for different elements
+         # Define colors for different elements
         colors = {'S': 'green', '.': 'white', '#': 'black', 'F': 'red', 'G': 'blue'}
         # Create a plot
         fig, ax = plt.subplots()
+
         # Plot each element with specified colors      
         for i in range(self.rows):
             for j in range(self.cols):
                 ax.text(i, j, self.grid[i][j], ha='center', va='center', color=colors[self.grid[i][j]])
+
         # Customize ticks
         ax.set_xticks(range(len(self.grid[0])))
         ax.set_yticks(range(len(self.grid)))
         ax.set_xticklabels([])
         ax.set_yticklabels([])
-
-        # Add description
+        
         plt.title('S - Start, G - Goal')
         plt.xlabel('# - Water Bodies')
         plt.ylabel('F - Flooded Roads')
 
         # Add grid lines
-        ax.grid(True, which='both', color='black', linewidth=1.5, linestyle='-', alpha=0.7)
-        plt.plot(*zip(*path), marker='o', color='red', label='Path')
+        ax.grid(True, which='both', color='black', linewidth=1.5, linestyle='-', alpha=0.3)
+        
+        col = ['red', 'green', 'blue', 'cyan', 'magenta', 'orange', 'purple']
+        colors = [col[random.randint(0, 6)] for _ in range(sys.getsizeof(path)-1)]
+        X, Y = zip(*path)
+        
+        # Plot the path with different colors for each segment
+        for i in range(sys.getsizeof(path)-1):
+            plt.plot(X[i:i+2], Y[i:i+2], color=colors[i])
+        
+        # plt.plot(*zip(*path), marker='o', color='red', label='Path')
         plt.scatter(*path[0], marker='o', color='green', label='Start')
         plt.scatter(*path[-1], marker='o', color='blue', label='Goal')
 
