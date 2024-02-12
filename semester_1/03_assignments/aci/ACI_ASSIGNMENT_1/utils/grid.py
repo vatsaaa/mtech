c46bd3f025b161577b97aef59b749b30d2b56b94
Ankur Vatsa <ankur.vatsa@gmail.com>
2024-02-12 15:06:51 +0530

    Fixing memory usage

--- semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/utils/grid.py
+++ semester_1/03_assignments/aci/ACI_ASSIGNMENT_1/utils/grid.py
@@ -3,34 +3,51 @@ import platform
 
 if platform.system() == "Windows":
     import psutil
-elif platform.system() == "Mac" or platform.system() == "Linux":
+elif platform.system() == "Darwin" or platform.system() == "Linux":
     import resource
 
-def track_time_and_space(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        start_memory = 0
-        end_memory = 0
+if platform.system() == "Windows":
+    def track_time_and_space(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            start_memory = psutil.Process().memory_info().rss 
 
-        if platform.system() == "Mac" or platform.system() == "Linux":
-            start_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        elif platform.system() == "Windows":
-            start_memory = psutil.Process().memory_info().rss
-
-        result = func(*args, **kwargs)
-        
-        end_time = time.time()
-        if platform.system == "Mac" or platform.system() == "Linux":
-            end_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        elif platform.system() == "Windows":
+            result = func(*args, **kwargs)
+            
+            end_time = time.time()
             end_memory = psutil.Process().memory_info().rss
 
-        execution_time = end_time - start_time
-        memory_usage = (end_memory - start_memory) / 1024  # Convert to kilobytes
+            print(platform.system(), start_memory, end_memory)
 
-        print(f"Execution time: {execution_time} seconds")
-        print(f"Memory usage: {memory_usage} KB")
+            execution_time = end_time - start_time
+            memory_usage = (end_memory - start_memory) / 1024  # Convert to kilobytes
 
-        return result
+            print(f"Execution time: {execution_time} seconds")
+            print(f"Memory usage: {memory_usage} KB")
 
-    return wrapper
+            return result, execution_time, memory_usage
+
+        return wrapper
+else:
+    def track_time_and_space(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            start_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+            result = func(*args, **kwargs)
+            
+            end_time = time.time()
+            end_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+            print(platform.system(), start_memory, end_memory)
+
+            execution_time = end_time - start_time
+            memory_usage = (end_memory - start_memory) / 1024  # Convert to kilobytes
+
+            print(f"Execution time: {execution_time} seconds")
+            print(f"Memory usage: {memory_usage} KB")
+
+            return result, execution_time, memory_usage
+        
+        return wrapper
+
